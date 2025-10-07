1681a06761a07d91a64245944126525b353b441b
hmontes <hugomontes927@gmail.com>
2025-10-07 17:59:25 -0400

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,6 @@ class DatabaseSeeder extends Seeder {
      */
     public function run(): void {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'admin@example.com',
-            'password' => '123456',
-            'role' => 'admin',
-        ]);
+        $this->call(UserSeeder::class);
     }
 }
