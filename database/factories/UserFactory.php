16b21d88b2d4c6d9c92751ad3dac7913f796afdf
hmontes <hugomontes927@gmail.com>
2025-10-08 19:30:29 -0400

    Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,8 +9,7 @@ use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
-{
+class UserFactory extends Factory {
     /**
      * The current password being used by the factory.
      */
@@ -21,23 +20,22 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => fake()->name(), // Genera nombres aleatorios
+            'email' => fake()->unique()->safeEmail(), // Genera correos aleatorios
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(10), // Genera una cadena aleatoria de 10 caracteres 
+            'role' => fake()->randomElement(['admin', 'member']), // Obtiene un valor aleatorio del array
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
+    public function unverified(): static {
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
