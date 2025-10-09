15a8573d72ba49aa8a86b551d0898d0f45164182
hmontes <hugomontes927@gmail.com>
2025-10-09 17:45:51 -0400

    Comentario Disqus

--- resources/views/web/movie-details.blade.php
+++ resources/views/web/movie-details.blade.php
@@ -108,6 +108,29 @@
                 </div>
                 <h4 style="margin-top: 20px; margin-bottom: 10px;">RESUMEN</h4>
                 <div>{!! $pelicula->resumen !!}</div>
+
+                <br />
+                <div id="disqus_thread"></div>
+                <script>
+                    /**
+                     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+                    /*
+                    var disqus_config = function () {
+                    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                    };
+                    */
+                    (function() { // DON'T EDIT BELOW THIS LINE
+                        var d = document,
+                            s = d.createElement('script');
+                        s.src = 'https://cinema-14.disqus.com/embed.js';
+                        s.setAttribute('data-timestamp', +new Date());
+                        (d.head || d.body).appendChild(s);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered
+                        by Disqus.</a></noscript>
             </div>
             <div class="col-md-3 side-bar">
                 <x-genero-list titulo="Todos los Géneros" :mostrar-contador="true" />
